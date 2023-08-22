e55c15ec17d51a3eb41334e7382b24388d8ebef6
rashidamahroof <rashidamahroofp@gmail.com>
2023-08-22 23:49:59 +0530

    s

--- js/main.js
+++ js/main.js
@@ -46,3 +46,27 @@ $('#owl-carousel').owlCarousel({
   nav: true,
   items: 2,
 })
+
+//partners section
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
