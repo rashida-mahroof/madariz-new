c2a09dce90f9c0aefcdd92e048aa3d5e2c7dec00
rashidamahroof <rashidamahroofp@gmail.com>
2023-08-24 07:05:29 +0530

    ;

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 // search-box open close js code
 let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
+let searchBox1 = document.querySelector(".search-box1 .bx-search");
+let searchBoxCancel1 = document.querySelector(".search-box1 .bx-x");
 let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
 searchBox.addEventListener("click", ()=>{
   navbar.classList.toggle("showInput");
   if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+searchBox1.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput1");
+  if(navbar.classList.contains("showInput1")){
     searchBox.classList.replace("bx-search" ,"bx-x");
   }else {
     searchBox.classList.replace("bx-x" ,"bx-search");
